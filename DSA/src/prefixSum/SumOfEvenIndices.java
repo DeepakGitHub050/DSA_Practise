81549e97a3e7f4d4ec55061fbdae5adb3c6b3320
DeepakGitHub050 <ds1008076@gmail.com>
2025-08-01 22:53:22 +0530

    Sum of even idices done

--- DSA/src/prefixSum/SumOfEvenIndices.java
+++ DSA/src/prefixSum/SumOfEvenIndices.java
@@ -1,7 +1,10 @@
 package prefixSum;
 
+import java.util.Arrays;
+
 /**
- * You are given an array A of length N and Q queries given by the 2D array B of size Q*2. Each query consists of two integers B[i][0] and B[i][1].
+ * You are given an array A of length N and Q queries given by the 2D array B of size Q*2.
+ * Each query consists of two integers B[i][0] and B[i][1].
  * For every query, the task is to calculate the sum of all even indices in the range A[B[i][0]…B[i][1]].
  *
  * Note : Use 0-based indexing
@@ -9,10 +12,34 @@ package prefixSum;
  */
 public class SumOfEvenIndices {
     public static void main(String[] args) {
-        int[] A = {1, 2, 3, 4, 5};
-        int[][] B = {{0,2},{1,4}};
-    }
-    static int[] solve() {
+        int[] A = {2, 1, 8, 3, 9};
+        int[][] B = {{0,3},{2,4}};
 
+        System.out.println(Arrays.toString(solve(A, B)));
+    }
+    static int[] solve(int[] A, int[][] B) {
+        int N = A.length;
+        int Q = B.length;
+        int[] res = new int[Q];
+        int[] pref = new int[N];
+        pref[0] = A[0];
+
+        for (int i=1; i<N; i++) {
+            if (i%2==0) {
+                pref[i] = pref[i-1] + A[i];
+            } else {
+                pref[i] = pref[i-1];
+            }
+        }
+
+        for (int i=0; i< Q; i++) {
+            int l = B[i][0];
+            int r = B[i][1];
+            if (l==0) {
+                res[i] = pref[r];
+            } else
+                res[i] = pref[r] - pref[l-1];
+        }
+        return res;
     }
 }
